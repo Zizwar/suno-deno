1c6459a58218887acee7a5c0b1ad4a9844e12d1c
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2024-07-25 04:51:35 +0100

    update honojs

--- suno-deno-hono.ts
+++ suno-deno-hono.ts
@@ -1,23 +1,26 @@
 import { Hono } from "npm:hono";
+import { cors } from "npm:hono/cors";
 import SunoDeno from "https://raw.githubusercontent.com/Zizwar/suno-deno/main/src/SunoDeno.js";
-//import SunoDeno from "npm:suno-deno";
-const SESS = Deno.env.get("SESS")||"sess_";
-const COOKIE = Deno.env.get("COOKIE")||"__client=eg";
 
-const Suno=async (c) => {
-  const sess = c.req.query("sess")||SESS;
-const cookie =c.req.query("cookie")||COOKIE;
+const SESS = Deno.env.get("SESS") || "sess_";
+const COOKIE = Deno.env.get("COOKIE") || "__client=eg";
 
-const suno = new SunoDeno(sess , cookie);
-await suno.init();
-return suno
-}
+const Suno = async (c) => {
+  const sess = c.req.query("sess") || SESS;
+  const cookie = c.req.query("cookie") || COOKIE;
+
+  const suno = new SunoDeno(sess, cookie);
+  await suno.init();
+  return suno;
+};
 
 const app = new Hono();
 
+// Add CORS middleware
+app.use("*", cors());
+
 app.get("/", async (c) => {
-  try{
-     // await suno.init();
+  try {
     return c.text(`# Suno-Deno
 Suno-Deno is a library designed for interacting with the Suno AI music generation service. 
 This library provides a comprehensive set of methods to generate songs, retrieve metadata, 
@@ -29,11 +32,9 @@ https://github.com/Zizwar/suno-deno`);
   }
 });
 
-//
-
 app.get("/limit", async (c) => {
   try {
- const suno = await Suno(c);   
+    const suno = await Suno(c);   
     const limit = await suno.getLimitLeft();
     return c.json({ limit });
   } catch (error) {
@@ -51,22 +52,53 @@ app.post("/generate-songs", async (c) => {
     return c.json({ error: error.message }, 500);
   }
 });
-app.post("/generate", async (c) => {
-    try {
-      const suno = await Suno(c);
-        const payload = await c.req.json();
-            const songs = await suno.getRequestIds(payload);
-                return c.json({ songs });
-                console.log({songs})
-                  } catch (error) {
-                      return c.json({ error: error.message }, 500);
-                        }
-                        });
 
-//})
+app.post("/generate", async (c) => {
+  try {
+    const suno = await Suno(c);
+    const payload = await c.req.json();
+    const songs = await suno.getRequestIds(payload);
+    console.log({songs});
+    return c.json({ songs });
+  } catch (error) {
+    return c.json({ error: error.message }, 500);
+  }
+});
+
+app.get("/search", async (c) => {
+  try {
+    const suno = await Suno(c);
+    //const payload = await c.req.json();
+ const q = c.req.query("q") || "gnawa";
+ const page = c.req.query("from_index") || 0;
+const rank = c.req.query("rank_by") || "trending";
+const name = "public_song"+q;
+ const payload = {
+    "search_queries": [
+        {
+            name,
+            "search_type": "public_song",
+            "term": q,
+            from_index:page,
+            rank_by:rank,
+            
+        }
+    ]
+}
+const data = await suno.search(payload) || [];
+//console.log(data)
+
+const {from_index=0,page_size=20,result:songs=[]} = data?.result[name] || [] 
+    //console.log({songs});
+    return c.json({from_index,page_size, songs });
+  } catch (error) {
+    return c.json({ error: error.message }, 500);
+  }
+});
+
 app.get("/metadata", async (c) => {
   try {
-const suno = await Suno(c);
+    const suno = await Suno(c);
     const ids = c.req.query("ids")?.split(",") || [];
     const metadata = await suno.getMetadata(ids);
     return c.json({ metadata });
@@ -76,10 +108,8 @@ const suno = await Suno(c);
 });
 
 app.get("/all-songs", async (c) => {
-
   try {
-const suno = await Suno(c);
-
+    const suno = await Suno(c);
     const songs = await suno.getAllSongs();
     return c.json({ songs });
   } catch (error) {
@@ -103,10 +133,9 @@ app.get("/song-buffer", async (c) => {
 });
 
 app.get("/generate-lyrics", async (c) => {
-
   try {
     const suno = await Suno(c);
-    const  prompt  = await c.req.query("prompt");
+    const prompt = await c.req.query("prompt");
     const lyrics = await suno.generateLyrics(prompt);
     return c.json({ lyrics });
   } catch (error) {
