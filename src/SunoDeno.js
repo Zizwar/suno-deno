5962d2464597ae01c68ddd09a32befde99a2b462
Zizwar <zizwar@gmail.com>
2024-08-12 23:59:00 +0000

    ghet user info get touch

--- src/SunoDeno.js
+++ src/SunoDeno.js
@@ -1,119 +1,148 @@
-const BASE_URL = 'https://studio-api.suno.ai';
+const BASE_URL = "https://studio-api.suno.ai";
 const MAX_RETRY_TIMES = 5;
 
 class SenoDeno {
-    constructor(sid, cookie) {
-        this.sid = sid;
-        this.cookie = cookie;
-        this.headers = {
-            "Accept-Encoding": "gzip, deflate, br",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-            "Cookie": cookie
-        };
-        this.retryTime = 0;
-        this.authUpdateTime = null;
+  constructor(sid, cookie) {
+    this.sid = sid;
+    this.cookie = cookie;
+    this.headers = {
+      "Accept-Encoding": "gzip, deflate, br",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+      Cookie: cookie,
+    };
+    this.retryTime = 0;
+    this.authUpdateTime = null;
+  }
 
+  async init() {
+    await this.renewAuth();
+  }
 
-    }
-
-
-
-    async init() {
-        await this.renewAuth();
-    }
-
-    async renewAuth() {
-        try {
-            const response = await fetch(`https://clerk.suno.com/v1/client/sessions/${this.sid}/tokens?_clerk_js_version=4.73.3`, {
-                method: 'POST',
-                headers: { Cookie: this.cookie }
-            });
-            if (!response.ok) throw new Error('Failed to renew auth token');
-            const data = await response.json();
-            const token = data?.jwt;
-            this.headers.Authorization = `Bearer ${token}`;
-            this.authUpdateTime = Date.now();
-        } catch (error) {
-            console.error('Error renewing auth token:', error);
-            throw error;
+  async renewAuth() {
+    try {
+      const response = await fetch(
+        `https://clerk.suno.com/v1/client/sessions/${this.sid}/tokens?_clerk_js_version=4.73.4`,
+        {
+          method: "POST",
+          headers: { Cookie: this.cookie },
         }
+      );
+      if (!response.ok) throw new Error("Failed to renew auth token");
+      const data = await response.json();
+      const token = data?.jwt;
+      this.headers.Authorization = `Bearer ${token}`;
+      this.authUpdateTime = Date.now();
+    } catch (error) {
+      console.error("Error renewing auth token:", error);
+      throw error;
     }
-
-    
-    async playlist(id,page=1) {
-        const response = await fetch(`${BASE_URL}/api/playlist/${id}?page=${page}`, {
-            headers: this.headers
-        });
-        const data = await response.json();
-        return data
-    }
-    async getLimitLeft() {
-        const response = await fetch(`${BASE_URL}/api/billing/info/`, {
-            headers: this.headers
-        });
-        const data = await response.json();
-        return Math.floor(data.total_credits_left / 10);
-    }
-
-    async getRequestIds(payload) {
-        if (!payload) throw new Error('Payload is required');
-        const response = await fetch(`${BASE_URL}/api/generate/v2/`, {
-            method: 'POST',
-            headers: {
-                ...this.headers,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(payload)
-        });
-        if (!response.ok) throw new Error(`Error response ${response.status}`);
-        const data = await response.json();
-return data;
-    }
-
-    async getMetadata(ids = []) {
-        const params = ids.length ? `?ids=${ids.join(',')}` : '';
-        let retryTimes = 0;
-
-        while (retryTimes <= MAX_RETRY_TIMES) {
-            const response = await fetch(`${BASE_URL}/api/feed/${params}`, {
-                headers: this.headers
-            });
-            const data = await response.json();
-return data;
-            if (data.some(item => item.id)) {
-                return data;
-            }
-
-            retryTimes += 1;
-            await new Promise(resolve => setTimeout(resolve, 5000));
+  }
+  async touch() {
+    try {
+      const response = await fetch(
+        `https://clerk.suno.com/v1/client/sessions/${this.sid}/touch?_clerk_js_version=4.73.4`,
+        {
+          method: "POST",
+          headers: { Cookie: this.cookie },
         }
+      );
+      if (!response.ok) throw new Error("Failed to get touch clerk");
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error get touch sessuib:", error);
+      throw error;
+    }
+  }
 
-        throw new Error('Failed to retrieve song metadata');
+  async playlist(id, page = 1) {
+    const response = await fetch(
+      `${BASE_URL}/api/playlist/${id}?page=${page}`,
+      {
+        headers: this.headers,
+      }
+    );
+    const data = await response.json();
+    return data;
+  }
+  async getCreatorInfo(id) {
+    const response = await fetch(
+      `${BASE_URL}/api/user/get-creator-info/${id}`,
+      {
+        headers: this.headers,
+      }
+    );
+    const data = await response.json();
+    return data;
+  }
+  async getLimitLeft() {
+    const response = await fetch(`${BASE_URL}/api/billing/info/`, {
+      headers: this.headers,
+    });
+    const data = await response.json();
+    return Math.floor(data.total_credits_left / 10);
+  }
+
+  async getRequestIds(payload) {
+    if (!payload) throw new Error("Payload is required");
+    const response = await fetch(`${BASE_URL}/api/generate/v2/`, {
+      method: "POST",
+      headers: {
+        ...this.headers,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+    if (!response.ok) throw new Error(`Error response ${response.status}`);
+    const data = await response.json();
+    return data;
+  }
+
+  async getMetadata(ids = []) {
+    const params = ids.length ? `?ids=${ids.join(",")}` : "";
+    let retryTimes = 0;
+
+    while (retryTimes <= MAX_RETRY_TIMES) {
+      const response = await fetch(`${BASE_URL}/api/feed/${params}`, {
+        headers: this.headers,
+      });
+      const data = await response.json();
+      return data;
+      if (data.some((item) => item.id)) {
+        return data;
+      }
+
+      retryTimes += 1;
+      await new Promise((resolve) => setTimeout(resolve, 5000));
     }
 
-    async generateSongs(payload) {
-        const requestIds = await this.getRequestIds(payload);
-        return await this.getMetadata(requestIds);
-    }
+    throw new Error("Failed to retrieve song metadata");
+  }
 
-    async getSongBuffer(url) {
-        const response = await fetch(url);
-        if (!response.ok) throw new Error(`Failed to download file from ${url}`);
-        const buffer = await response.arrayBuffer();
-        return buffer;
-    }
+  async generateSongs(payload) {
+    const requestIds = await this.getRequestIds(payload);
+    return await this.getMetadata(requestIds);
+  }
 
-    async getAllSongs() {
-        return await this.getMetadata();
-    }
-    async search(payload) {
-        const response = await fetch(`${BASE_URL}/api/search/`, {
-            method: 'POST',
-            headers: {
-                ...this.headers,
-                'Content-Type': 'application/json'
-            },
-            /*
+  async getSongBuffer(url) {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`Failed to download file from ${url}`);
+    const buffer = await response.arrayBuffer();
+    return buffer;
+  }
+
+  async getAllSongs() {
+    return await this.getMetadata();
+  }
+  async search(payload) {
+    const response = await fetch(`${BASE_URL}/api/search/`, {
+      method: "POST",
+      headers: {
+        ...this.headers,
+        "Content-Type": "application/json",
+      },
+      /*
             const payload = {
            "search_queries": [
             {
@@ -126,33 +155,32 @@ return data;
               ]
              }
             */
-            body: JSON.stringify(payload)
+      body: JSON.stringify(payload),
+    });
+    const data = await response.json();
+    return data;
+  }
+  async generateLyrics(prompt) {
+    const response = await fetch(`${BASE_URL}/api/generate/lyrics/`, {
+      method: "POST",
+      headers: {
+        ...this.headers,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt }),
+    });
+    const data = await response.json();
+    const id = data?.id;
 
-        });
-        const data = await response.json();
-        return data;
+    while (true) {
+      const response = await fetch(`${BASE_URL}/api/generate/lyrics/${id}`, {
+        headers: this.headers,
+      });
+      const result = await response.json();
+      if (result.status === "complete") return result;
+      await new Promise((resolve) => setTimeout(resolve, 300));
     }
-    async generateLyrics(prompt) {
-        const response = await fetch(`${BASE_URL}/api/generate/lyrics/`, {
-            method: 'POST',
-            headers: {
-                ...this.headers,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ prompt })
-        });
-        const data = await response.json();
-        const id = data?.id;
-
-        while (true) {
-            const response = await fetch(`${BASE_URL}/api/generate/lyrics/${id}`, {
-                headers: this.headers
-            });
-            const result = await response.json();
-            if (result.status === 'complete') return result;
-            await new Promise(resolve => setTimeout(resolve, 300));
-        }
-    }
+  }
 }
 
 export default SenoDeno;
